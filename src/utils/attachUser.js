b4c7815942aa0e51591f15868c8c94e187e01a99
Mayank Saxena <mayanksaxenaah.0714@gmail.com>
2026-02-21 19:58:15 +0530

    changed file

--- src/utils/attachUser.js
+++ src/utils/attachUser.js
@@ -2,12 +2,16 @@ import { findUserById } from "../dao/user.dao.js"
 import { verifyToken } from "./helper.js"
 
 export const attachUser = async (req, res, next) => {
+    console.log("Cookies received:", req.cookies);
     const token = req.cookies.accessToken
+    console.log("Token:", token);
     if(!token) return next()
 
     try {
         const decoded = verifyToken(token)
+        console.log("Decoded:", decoded)
         const user = await findUserById(decoded)
+        console.log("User found:", user)
         console.log("TOKEN:", token)
         console.log("Decoded token:", decoded)
         console.log("User found:", user)
@@ -16,6 +20,7 @@ export const attachUser = async (req, res, next) => {
         next()
     } catch (error) {
         console.log(error)
+        console.log("Verify error:", error)
         next()
     }
 }
